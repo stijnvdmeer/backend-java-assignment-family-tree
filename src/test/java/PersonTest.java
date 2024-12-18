8af1333939065f90aaedbdacb08055758e6ea12f
stijnvdmeer <stijn08072002@outlook.com>
2024-12-18 16:44:00 +0100

    Added Person Tests

---
+++ src/test/java/PersonTest.java
@@ -0,0 +1,157 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PersonTest {
+    @Test
+    public void canGetPersonName() {
+        // Arrange
+        Person p = new Person("Stijn", "SmallS", "van der Meer", "male", 22);
+        // Act
+        String personName = p.getName();
+
+        // Assert
+        assertEquals("Stijn", personName);
+    }
+
+    @Test
+    public void canGetPersonMiddleName() {
+        // Arrange
+        Person p = new Person("Stijn", "SmallS", "van der Meer", "male", 22);
+        // Act
+        String personMiddleName = p.getMiddleName();
+
+        // Assert
+        assertEquals("SmallS", personMiddleName);
+    }
+
+    @Test
+    public void canGetPersonLastName() {
+        // Arrange
+        Person p = new Person("Stijn", "SmallS", "van der Meer", "male", 22);
+        // Act
+        String personLastName = p.getLastName();
+
+        // Assert
+        assertEquals("van der Meer", personLastName);
+    }
+    @Test
+    public void canGetPersonSex() {
+        // Arrange
+        Person p = new Person("Stijn", "SmallS", "van der Meer", "male", 22);
+        // Act
+        String personSex = p.getSex();
+
+        // Assert
+        assertEquals("male", personSex);
+    }
+    @Test
+    public void canGetPersonAge() {
+        // Arrange
+        Person p = new Person("Stijn", "SmallS", "van der Meer", "male", 22);
+        // Act
+        int personAge = p.getAge();
+
+        // Assert
+        assertEquals(22, personAge);
+    }
+    @Test
+    public void canSetPersonName() {
+        // Arrange
+        Person p = new Person("Stijn", "SmallS", "van der Meer", "male", 22);
+
+        // Act
+        p.setName("Daan");
+
+        // Assert
+        assertEquals("Daan", p.getName());
+    }
+    @Test
+    public void canSetPersonMiddleName() {
+        // Arrange
+        Person p = new Person("Stijn", "SmallS", "van der Meer", "male", 22);
+
+        // Act
+        p.setMiddleName("Casper");
+
+        // Assert
+        assertEquals("Casper", p.getMiddleName());
+    }
+    @Test
+    public void canSetPersonLastName() {
+        // Arrange
+        Person p = new Person("Stijn", "SmallS", "van der Meer", "male", 22);
+
+        // Act
+        p.setLastName("Groot");
+
+        // Assert
+        assertEquals("Groot", p.getLastName());
+    }
+    @Test
+    public void canSetPersonSex() {
+        // Arrange
+        Person p = new Person("Stijn", "SmallS", "van der Meer", "male", 22);
+
+        // Act
+        p.setSex("female");
+
+        // Assert
+        assertEquals("female", p.getSex());
+    }
+    @Test
+    public void canSetPersonAge() {
+        // Arrange
+        Person p = new Person("Stijn", "SmallS", "van der Meer", "male", 22);
+
+        // Act
+        p.setAge(23);
+
+        // Assert
+        assertEquals(23, p.getAge());
+    }
+
+    @Test
+    public void canSetAndGetMother() {
+        // Arrange
+        Person p = new Person("Stijn", "SmallS", "van der Meer", "male", 22);
+        Person mama = new Person("Anja", "Groot", "female", 55);
+
+        // Act
+        p.setMother(mama);
+        String MotherName = p.getMother().getName();
+
+        // Assert
+        assertEquals("Anja", MotherName);
+    }
+    @Test
+    public void canSetAndGetFather() {
+        // Arrange
+        Person p = new Person("Stijn", "SmallS", "van der Meer", "male", 22);
+        Person papa = new Person("Bauke", "van der Meer", "male", 60);
+
+        // Act
+        p.setFather(papa);
+        String fatherName = p.getFather().getName();
+
+        // Assert
+        assertEquals("Bauke", fatherName);
+    }
+    @Test
+    public void canSetAndGetSiblings() {
+        // Arrange
+        Person p = new Person("Stijn", "SmallS", "van der Meer", "male", 22);
+        Person b1 = new Person("Daan", "van der Meer", "male", 26);
+        Person b2 = new Person("Jelle", "van der Meer", "male", 30);
+
+        // Act
+        p.addSibling(b1);
+        p.addSibling(b2);
+        String siblingName = p.getSiblings().get(0).getName();
+
+        // Assert
+        assertEquals("Daan", siblingName);
+    }
+    @Test
+    public void canSetAndGetChildren() {}
+}
