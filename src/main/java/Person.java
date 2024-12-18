0c2d495707d7b965dfe4a17bdd67d3fa8b21c9fe
stijnvdmeer <stijn08072002@outlook.com>
2024-12-18 15:40:50 +0100

    initial

---
+++ src/main/java/Person.java
@@ -0,0 +1,136 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Person {
+    // Variables
+    private String name;
+    private String middleName;
+    private String lastName;
+    private String sex;
+    private int age;
+    private Person mother;
+    private Person father;
+    private List<Person> siblings;
+    private List<Person> children;
+    private List<Pet> pets;
+
+    // Constructors
+    public Person(String name, String lastName, String sex, int age) {
+        this.name = name;
+        this.lastName = lastName;
+        this.sex = sex;
+        this.age = age;
+    }
+    public Person(String name, String middleName, String lastName, String sex, int age) {
+        this.name = name;
+        this.middleName = middleName;
+        this.lastName = lastName;
+        this.sex = sex;
+        this.age = age;
+    }
+
+    // Getters
+    public String getName() {
+        return name;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public Person getMother() {
+        return mother;
+    }
+
+    public Person getFather() {
+        return father;
+    }
+
+    public List<Person> getSiblings() {
+        return siblings;
+    }
+
+    public List<Person> getChildren() {
+        return children;
+    }
+
+    public List<Pet> getPets() {
+        return pets;
+    }
+
+    // Setters
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setMother(Person mother) {
+        this.mother = mother;
+    }
+
+    public void setFather(Person father) {
+        this.father = father;
+    }
+
+    public void setSiblings(List<Person> siblings) {
+        this.siblings = siblings;
+    }
+
+    public void setChildren(List<Person> children) {
+        this.children = children;
+    }
+
+    public void setPets(List<Pet> pets) {
+        this.pets = pets;
+    }
+
+    // Methods
+    public void addParents(Person mother, Person father) {
+        this.mother = mother;
+        this.father = father;
+    }
+    public void addChild(Person child) {
+        this.children.add(child);
+    }
+    public void addPet(Pet pet) {
+        pet.setOwner(this);
+        this.pets.add(pet);
+    }
+    public void addSibling(Person sibling) {
+        this.siblings.add(sibling);
+    }
+    public List<Person> getGrandChildren() {
+        List<Person> grandChildren = new ArrayList<Person>();
+        for(Person child : children) {
+            grandChildren.addAll(0, child.children);
+        }
+        return grandChildren;
+    }
+}
---
+++ src/main/java/Pet.java
@@ -0,0 +1,51 @@
+public class Pet {
+    // Variables
+    private String name;
+    private int age;
+    private String species;
+    private Person owner;
+
+    // Constructors
+    public Pet(String name, int age, String species) {
+        this.name = name;
+        this.age = age;
+        this.species = species;
+    }
+
+    // Getters
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getSpecies() {
+        return species;
+    }
+
+    public Person getOwner() {
+        return owner;
+    }
+
+    // Setters
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setSpecies(String species) {
+        this.species = species;
+    }
+
+    public void setOwner(Person owner) {
+        this.owner = owner;
+    }
+    // Methods
+
+
+}
